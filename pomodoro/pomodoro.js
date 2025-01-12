a35e7fafa160b1d47cd3e05106204787563361c0
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-01-11 19:29:23 -0600

    pomodoro js

--- pomodoro/pomodoro.js
+++ pomodoro/pomodoro.js
@@ -1,15 +1,21 @@
-const isRunning = false; // Tracks if timer is currently running
-let timeLeft; // Remaining time in seconds
-const workTime = 25; // Work duration in minutes
-const breakTime = 5; // Break duration in minutes
-const isWorkTime = true; // Tracks if we're working or in break mode
+const isRunning = false; //tracks if timer is currently running
+let timeleft ; //remaining time in seconds
+const workTime = 25;
+const breakTime = 5;
+const isWorkTime = true; //checks if we are working or in break
+
+// const mins = document.getElementById -this is one way to get make a variable in JavaScript..here it is a const so you can only define it one way
+// let mins = document.getElementById - another way
+// var mins = document.getElementById - the third way but very old wau
+// so keywords in javascript are const, let and var
 
 const minutesDisplay = document.getElementById("minutes");
 const secondsDisplay = document.getElementById("seconds");
 const startButton = document.getElementById("start");
 const pauseButton = document.getElementById("pause");
 const resetButton = document.getElementById("reset");
-const workTimeInput = document.getElementById("work-time");
-const breakTimeInput = document.getElementById("break-time");
+const workTimeinput = document.getElementById("work_time");
+const breakTimeinput = document.getElementById("break_time");
 const statusDisplay = document.getElementById("status");
 const progressBar = document.getElementById("progress");
+
